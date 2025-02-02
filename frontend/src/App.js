d1b001eb8b97314e466762af1ee90c6213a13286
KarthickAravind <karthickaravindofficial@gmail.com>
2025-02-02 09:10:32 +0530

    Final Reupload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
   const fetchWeather = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://weather-backend.onrender.com/weather/', { name: city });
+      const response = await axios.post('https://my-weather-dashboard-egau.onrender.com', { name: city });
       setWeather(response.data);
       setError('');
     } catch (error) {
