c463c47cc4cc449638861e1c27e8634c48d67aee
KarthickAravind <karthickaravindofficial@gmail.com>
2025-02-02 09:17:25 +0530

    version-2.0

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ def read_root():
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow your React app's origin
+    allow_origins=["https://weather-frontend-nwhg.onrender.com"],  # Allow your React app's origin
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods
     allow_headers=["*"],  # Allow all headers
